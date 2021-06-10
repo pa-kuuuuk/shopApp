7b2a1ae77d97f8357f0d9dac026bb4273bdd4e16
pa-kuuuuk <parkjungyong0522@gmail.com>
2021-06-10 17:40:36 +0900

    더보기 버튼  까지

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,31 +1,60 @@
 import React, { useEffect, useState } from 'react'
 import { FaCode } from "react-icons/fa";
 import axios from 'axios';
-import {Col, Card,Row, Carousel} from "antd";
+import {Col, Card,Row, Carousel, Button} from "antd";
 import { RocketOutlined } from "@ant-design/icons";
 import Meta from 'antd/lib/card/Meta';
 import ImageSilder from '../../utils/ImageSilder';
 
 function LandingPage() {
 
-    const [Products, setProducts] = useState([])
+    const [Products, setProducts] = useState([]);
+    const [Skip, setSkip] = useState(0);
+    const [Limit, setLimit] = useState(2);
+    const [PostSize, setPostSize] = useState(0);
+
 
     //데이터베이스에서 상품 가져오기 랜더링 됨과 동시에 실행
     useEffect(() => {
+        let body ={
+            skip:Skip,
+            limit:Limit
+        }
+        
+        getProducts(body);
 
-
-
-
-        //일단 모든 상품을 요청한다.
-        axios.post('/api/product/products')
-            .then(response => {
-                if(response.data.success){
-                    setProducts(response.data.productInfo);
-                }else{
-                    alert("상품목록을 가져오는데 실패 했습니다.")
-                }
-            })
     },[]);
+
+    const getProducts = (body) => {
+        axios.post('/api/product/products', body)
+        .then(response => {
+            if(response.data.success){
+                if(body.loadMore){
+                    setProducts([...Products, ...response.data.productInfo ])
+                } else{
+                    setProducts(response.data.productInfo);
+                }
+                setPostSize(response.data.postSize)
+            }else{
+                alert("상품목록을 가져오는데 실패 했습니다.")
+            }
+        })
+    }
+
+    const loadMoreHandler = () => {
+        
+        let skip = Skip+Limit;
+        
+        setSkip(skip);
+        
+        let body ={
+            skip:skip,
+            limit:Limit,
+            loadMore: true
+        }
+        
+        getProducts(body);
+    }
 
     const renderCards = Products.map((product, index) => {
         return  <Col lg={6} md={8} xs={24}  key={index}> 
@@ -54,11 +83,14 @@ function LandingPage() {
             <Row gutter={[16,16]}>
                 {renderCards}
             </Row>
-
-
-            <div style={{display:'flex', justifyContent: 'center'}}>
-                <button>더보기</button>
-            </div>
+            
+            <br/>
+            
+            {PostSize >= Limit &&
+                <div style={{display:'flex', justifyContent: 'center'}}>
+                    <Button onClick={loadMoreHandler}>더보기</Button>
+                </div>
+            }
         </div>
     )
 }
